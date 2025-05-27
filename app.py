9827f35dc35d4f4b9b0d4806a97f5171bcd05621
shubham <shubhamgup87749@gmail.com>
2025-05-27 17:14:38 +0530

    changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session, abort
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
-import os
+import os 
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
